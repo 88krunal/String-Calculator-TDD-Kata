7c658b5cae86f4ae43211605d524de9776f01aba
Krunal Agrawal <agrawalkrunal04022001@gmail.com>
2021-08-28 11:18:42 +0530

    Refactor after Test No Multiple Negative Numbers

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
-
+import java.util.stream.Stream;
 public class Calculator {
 
 	private String delimiter;
@@ -18,20 +17,26 @@ public class Calculator {
 	}
 	
 	private void NoNegativeNumbers() {
-
-		if(getNumbers().anyMatch(n -> n<0)) {
-			String NegativenumberSequence=getNumbers().filter(n -> n<0).mapToObj(Integer::toString)
-					.collect(Collectors.joining(","));
+		String NegativenumberSequence=getNumbers().filter(n -> n<0)
+				.mapToObj(Integer::toString)
+				.collect(Collectors.joining(","));
+		 
+		if(!NegativenumberSequence.isEmpty()) {
 			throw new IllegalArgumentException("negatives not allowed: "+NegativenumberSequence);
 		}
 	}
+	
 	private IntStream getNumbers() {
-		return Arrays.stream(numbers.split(delimiter))
+		if(numbers.isEmpty()) {
+			return IntStream.empty();
+		}
+		else {
+			return Stream.of(numbers.split(delimiter))
 				.mapToInt(Integer::parseInt);
+		}
 	}
+	
 	public static int add(String input) {
-		if(input.isEmpty())
-			return 0;
 		return parseinput(input).addUtil();
 	}
 	
