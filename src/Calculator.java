0116c3e5d873489b2ede0d0a75d61570a3d942f0
Krunal Agrawal <agrawalkrunal04022001@gmail.com>
2021-08-28 10:13:35 +0530

    Refactor after test case pass different delimiter

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,33 @@
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Calculator {
 
+	private String delimiter;
+	private String numbers;
+	
+	private Calculator(String delimiter,String numbers) {
+		this.delimiter=delimiter;
+		this.numbers=numbers;
+	}
+	
+	private int addUtil() {
+		return Arrays.stream(numbers.split(delimiter))
+				.mapToInt(Integer::parseInt).sum();
+	}
+	
 	public static int add(String input) {
 		if(input.isEmpty())
 			return 0;
-		String delimiter=",|\n";
+		return parseinput(input).addUtil();
+	}
+	
+	private static Calculator parseinput(String input) {
 		if(input.startsWith("//")) {
 			String inputparts[]=input.split("\n",2);
-			delimiter=inputparts[0].substring(2);
-			input=inputparts[1];
+			return new Calculator(inputparts[0].substring(2),inputparts[1]);
 		}
-		Stream<String> Numbers= Arrays.stream(input.split(delimiter));
-		return Numbers.mapToInt(Integer::parseInt).sum();
-	
+		else {
+			return new Calculator(",|\n",input);
+		}
 	}
 }
