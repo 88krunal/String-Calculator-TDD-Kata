c83f8ba64c142125f3d15465bc65088fb799f1a5
Krunal Agrawal <agrawalkrunal04022001@gmail.com>
2021-08-28 09:43:55 +0530

    Test Number String New Line By Delimiter Pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 	public static int add(String input) {
 		if(input.isEmpty())
 			return 0;
-		Stream<String> Numbers= Arrays.stream(input.split(","));
+		Stream<String> Numbers= Arrays.stream(input.split(",|\n"));
 		return Numbers.mapToInt(Integer::parseInt).sum();
 	
 	}
