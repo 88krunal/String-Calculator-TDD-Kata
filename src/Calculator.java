3fa7dbe5568bef498d6eb8eb011ed9e099954857
Krunal Agrawal <agrawalkrunal04022001@gmail.com>
2021-08-28 09:56:16 +0530

    Test Different Delimiter case Pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,13 @@ public class Calculator {
 	public static int add(String input) {
 		if(input.isEmpty())
 			return 0;
-		Stream<String> Numbers= Arrays.stream(input.split(",|\n"));
+		String delimiter=",|\n";
+		if(input.startsWith("//")) {
+			String inputparts[]=input.split("\n",2);
+			delimiter=inputparts[0].substring(2);
+			input=inputparts[1];
+		}
+		Stream<String> Numbers= Arrays.stream(input.split(delimiter));
 		return Numbers.mapToInt(Integer::parseInt).sum();
 	
 	}
